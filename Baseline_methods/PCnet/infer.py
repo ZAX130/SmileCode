f6524a5d4ca0e91b79bf56a1fd0e991c8ed71d85
KuangSan <43944700+ZAX130@users.noreply.github.com>
2023-06-29 14:50:26 +0800

    Add files via upload

---
+++ Baseline_methods/PCnet/infer.py
@@ -0,0 +1,118 @@
+import glob
+import os, losses, utils
+from torch.utils.data import DataLoader
+from data import datasets, trans
+import numpy as np
+import torch
+from torchvision import transforms
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from natsort import natsorted
+from models import ModeT
+import random
+def same_seeds(seed):
+    # Python built-in random module
+    random.seed(seed)
+    # Numpy
+    np.random.seed(seed)
+    # Torch
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    # torch.backends.cudnn.deterministic = True
+
+same_seeds(24)
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+        self.vals = []
+        self.std = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        self.vals.append(val)
+        self.std = np.std(self.vals)
+
+def main():
+
+    val_dir = '/LPBA_path/Val/'
+    weights = [1, 1]  # loss weights
+    lr = 0.0001
+    head_dim = 6
+    num_heads = [8, 4, 2, 1, 1]
+    model_idx = -1
+    model_folder = 'modet-heads({}{}{}{}{})-rpe_headim_{}_ncc_{}_reg_{}_lr_{}_54r/'.format(*num_heads, head_dim, weights[0], weights[1], lr)
+    model_dir = 'experiments/' + model_folder
+
+    img_size = (160, 192, 160)
+    model = ModeT(img_size, head_dim=head_dim, num_heads=num_heads, scale=1)
+    best_model = torch.load(model_dir + natsorted(os.listdir(model_dir))[model_idx])['state_dict']
+    print('Best model: {}'.format(natsorted(os.listdir(model_dir))[model_idx]))
+    model.load_state_dict(best_model)
+    model.cuda()
+    reg_model = utils.register_model(img_size, 'nearest')
+    reg_model.cuda()
+    test_composed = transforms.Compose([trans.Seg_norm(),
+                                        trans.NumpyType((np.float32, np.int16)),
+                                        ])
+    test_set = datasets.LPBABrainInferDatasetS2S(glob.glob(val_dir + '*.pkl'), transforms=test_composed)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=0, pin_memory=True, drop_last=True)
+    eval_dsc_def = AverageMeter()
+    eval_dsc_raw = AverageMeter()
+    eval_det = AverageMeter()
+    with torch.no_grad():
+        stdy_idx = 0
+        for data in test_loader:
+            model.eval()
+            data = [t.cuda() for t in data]
+            x = data[0]
+            y = data[1]
+            x_seg = data[2]
+            y_seg = data[3]
+
+            x_def, flow = model(x,y)
+            def_out = reg_model([x_seg.cuda().float(), flow.cuda()])
+            tar = y.detach().cpu().numpy()[0, 0, :, :, :]
+            jac_det = utils.jacobian_determinant_vxm(flow.detach().cpu().numpy()[0, :, :, :, :])
+            eval_det.update(np.sum(jac_det <= 0) / np.prod(tar.shape), x.size(0))
+            dsc_trans = utils.dice_val_VOI(def_out.long(), y_seg.long())
+            dsc_raw = utils.dice_val_VOI(x_seg.long(), y_seg.long())
+            print('Trans dsc: {:.4f}, Raw dsc: {:.4f}'.format(dsc_trans.item(),dsc_raw.item()))
+            eval_dsc_def.update(dsc_trans.item(), x.size(0))
+            eval_dsc_raw.update(dsc_raw.item(), x.size(0))
+            stdy_idx += 1
+        print('Deformed DSC: {:.3f} +- {:.3f}, Affine DSC: {:.3f} +- {:.3f}'.format(eval_dsc_def.avg,
+                                                                                    eval_dsc_def.std,
+                                                                                    eval_dsc_raw.avg,
+                                                                                    eval_dsc_raw.std))
+        print('deformed det: {}, std: {}'.format(eval_det.avg, eval_det.std))
+
+
+if __name__ == '__main__':
+    '''
+    GPU configuration
+    '''
+    GPU_iden = 0
+    GPU_num = torch.cuda.device_count()
+    print('Number of GPU: ' + str(GPU_num))
+    for GPU_idx in range(GPU_num):
+        GPU_name = torch.cuda.get_device_name(GPU_idx)
+        print('     GPU #' + str(GPU_idx) + ': ' + GPU_name)
+    torch.cuda.set_device(GPU_iden)
+    GPU_avai = torch.cuda.is_available()
+    print('Currently using: ' + torch.cuda.get_device_name(GPU_iden))
+    print('If the GPU is available? ' + str(GPU_avai))
+    main()
---
+++ Baseline_methods/PCnet/utils.py
@@ -0,0 +1,150 @@
+import math
+import numpy as np
+import torch.nn.functional as F
+import torch, sys
+from torch import nn
+import pystrum.pynd.ndutils as nd
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+        self.vals = []
+        self.std = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        self.vals.append(val)
+        self.std = np.std(self.vals)
+
+
+class SpatialTransformer(nn.Module):
+    """
+    N-D Spatial Transformer
+    """
+
+    def __init__(self, size, mode='bilinear'):
+        super().__init__()
+
+        self.mode = mode
+
+        # create sampling grid
+        vectors = [torch.arange(0, s) for s in size]
+        grids = torch.meshgrid(vectors)
+        grid = torch.stack(grids)
+        grid = torch.unsqueeze(grid, 0)
+        grid = grid.type(torch.FloatTensor).cuda()
+
+        # registering the grid as a buffer cleanly moves it to the GPU, but it also
+        # adds it to the state dict. this is annoying since everything in the state dict
+        # is included when saving weights to disk, so the model files are way bigger
+        # than they need to be. so far, there does not appear to be an elegant solution.
+        # see: https://discuss.pytorch.org/t/how-to-register-buffer-without-polluting-state-dict
+        self.register_buffer('grid', grid)
+
+    def forward(self, src, flow):
+        # new locations
+        new_locs = self.grid + flow
+        shape = flow.shape[2:]
+
+        # need to normalize grid values to [-1, 1] for resampler
+        for i in range(len(shape)):
+            new_locs[:, i, ...] = 2 * (new_locs[:, i, ...] / (shape[i] - 1) - 0.5)
+
+        # move channels dim to last position
+        # also not sure why, but the channels need to be reversed
+        if len(shape) == 2:
+            new_locs = new_locs.permute(0, 2, 3, 1)
+            new_locs = new_locs[..., [1, 0]]
+        elif len(shape) == 3:
+            new_locs = new_locs.permute(0, 2, 3, 4, 1)
+            new_locs = new_locs[..., [2, 1, 0]]
+
+        return F.grid_sample(src, new_locs, align_corners=True, mode=self.mode)
+
+class register_model(nn.Module):
+    def __init__(self, img_size=(64, 256, 256), mode='bilinear'):
+        super(register_model, self).__init__()
+        self.spatial_trans = SpatialTransformer(img_size, mode)
+
+    def forward(self, x):
+        img = x[0].cuda()
+        flow = x[1].cuda()
+        out = self.spatial_trans(img, flow)
+        return out
+
+
+def dice_val_VOI(y_pred, y_true):
+    VOI_lbls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+                36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+                48, 49, 50, 51, 52, 53, 54]
+
+    pred = y_pred.detach().cpu().numpy()[0, 0, ...]
+    true = y_true.detach().cpu().numpy()[0, 0, ...]
+    DSCs = np.zeros((len(VOI_lbls), 1))
+    idx = 0
+    for i in VOI_lbls:
+        pred_i = pred == i
+        true_i = true == i
+        intersection = pred_i * true_i
+        intersection = np.sum(intersection)
+        union = np.sum(pred_i) + np.sum(true_i)
+        dsc = (2.*intersection) / (union + 1e-5)
+        DSCs[idx] =dsc
+        idx += 1
+    return np.mean(DSCs)
+
+def jacobian_determinant_vxm(disp):
+    """
+    jacobian determinant of a displacement field.
+    NB: to compute the spatial gradients, we use np.gradient.
+    Parameters:
+        disp: 2D or 3D displacement field of size [*vol_shape, nb_dims],
+              where vol_shape is of len nb_dims
+    Returns:
+        jacobian determinant (scalar)
+    """
+
+    # check inputs
+    disp = disp.transpose(1, 2, 3, 0)
+    volshape = disp.shape[:-1]
+    nb_dims = len(volshape)
+    assert len(volshape) in (2, 3), 'flow has to be 2D or 3D'
+
+    # compute grid
+    grid_lst = nd.volsize2ndgrid(volshape)
+    grid = np.stack(grid_lst, len(volshape))
+
+    # compute gradients
+    J = np.gradient(disp + grid)
+
+    # 3D glow
+    if nb_dims == 3:
+        dx = J[0]
+        dy = J[1]
+        dz = J[2]
+
+        # compute jacobian components
+        Jdet0 = dx[..., 0] * (dy[..., 1] * dz[..., 2] - dy[..., 2] * dz[..., 1])
+        Jdet1 = dx[..., 1] * (dy[..., 0] * dz[..., 2] - dy[..., 2] * dz[..., 0])
+        Jdet2 = dx[..., 2] * (dy[..., 0] * dz[..., 1] - dy[..., 1] * dz[..., 0])
+
+        return Jdet0 - Jdet1 + Jdet2
+
+    else:  # must be 2
+
+        dfdx = J[0]
+        dfdy = J[1]
+
+        return dfdx[..., 0] * dfdy[..., 1] - dfdy[..., 0] * dfdx[..., 1]
